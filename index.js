2dd433cbb303493011294842237977a4cd18f417
William Melo <78105093+william-melo@users.noreply.github.com>
2024-08-02 21:38:02 -0500

    pagina principal

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-import dotenv from "dotenv";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 import express from "express";
 import cors from "cors";
+import path from "path";
 import { clientsRouter } from "./routes/clients.js";
 
-dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 
@@ -11,7 +14,16 @@ app.use(express.json());
 app.use(cors());
 app.disable("x-powered-by");
 
+// Sirve archivos estáticos desde la carpeta 'public'
+app.use(express.static(path.join(__dirname, "public")));
+
+// Define las rutas de tu API
 app.use("/clients", clientsRouter);
+
+// Ruta para servir tu archivo HTML principal
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 const PORT = process.env.PORT ?? 1234;
 
